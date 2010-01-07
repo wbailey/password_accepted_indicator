856e606ff112b38e1dc6ef3be16df0f70c550cdf
wbailey <wes@verticalresponse.com>
2010-01-07 15:24:02 -0800

    following the helper convention

--- lib/password_accepted_indicator.rb
+++ lib/password_accepted_indicator.rb
@@ -1,14 +1,14 @@
-module PasswordAcceptedIndicator
+module PasswordAcceptedIndicatorHelper
   def password_accepted_indicator( name, target, options = { } )
     width = options.has_key?( :width ) ? option[:width] : 40
     return <<-EOF
     <div id="#{name}" class="password_accepted_indicator">
-      <div id="#{name + 'indicator_box'}" class="box_container" style="width: #{width}">&nbsp;</div>
+      <div id="#{name + 'indicator_box'}" class="box_container_denied" style="width: #{width}">&nbsp;</div>
       <div id="#{name + 'indicator_text'}" class="text_container"></div>
     </div>
     <script type="text/javascript">
     //<![CDATA[
-    $('#{target}').observe('keyup', function(event){check_password_strength('#{target}', '#{name}')}); $('#{name}_indicator_text').innerHTML = 'Denied';
+    $('#{target}').observe( 'keyup', function( event ) { password_is_accepted( '#{target}', '#{name}' ) } ); $('#{name}_indicator_text').innerHTML = 'Denied';
     //]]>
     </script>
     EOF
