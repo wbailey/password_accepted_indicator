6736831d717641966004a4c28d2b7f0bf6d18c11
wbailey <wes@verticalresponse.com>
2010-01-07 16:03:46 -0800

    helps if you have the right files present

--- init.rb
+++ init.rb
@@ -1,3 +1,5 @@
-ActionView::Base.send :include, PasswordAcceptedInidicatorHelper
+require 'password_accepted_indicator'
+
+ActionView::Base.send :include, PasswordAcceptedIndicatorHelper
 
 ActionView::Helpers::AssetTagHelper.register_javascript_include_default 'password_accepted_indicator'
