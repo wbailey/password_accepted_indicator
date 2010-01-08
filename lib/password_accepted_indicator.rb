f52dd70a4858fe774cdee5581a6dbf9700d1c9f4
wbailey <wes@verticalresponse.com>
2010-01-07 16:07:39 -0800

    cleaned up css issues with the appropriate id and class names

--- lib/password_accepted_indicator.rb
+++ lib/password_accepted_indicator.rb
@@ -3,8 +3,8 @@ module PasswordAcceptedIndicatorHelper
     width = options.has_key?( :width ) ? option[:width] : 40
     return <<-EOF
     <div id="#{name}" class="password_accepted_indicator">
-      <div id="#{name + 'indicator_box'}" class="box_container_denied" style="width: #{width}">&nbsp;</div>
-      <div id="#{name + 'indicator_text'}" class="text_container"></div>
+      <div id="#{name + '_indicator_box'}" class="box_container" style="width: #{width}">&nbsp;</div>
+      <div id="#{name + '_indicator_text'}" class="text_container"></div>
     </div>
     <script type="text/javascript">
     //<![CDATA[
